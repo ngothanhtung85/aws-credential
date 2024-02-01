c5405f0f0b5b1f154ce0e1d63e0755c9400f8e13
TUNG NGO <ngothanhtung85@gmail.com>
2024-02-01 17:27:41 +0700

    update

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  function_name = "lambda_function_name_tungngo01"
   role          = "arn:aws:iam::327669278944:role/lambda-excute-credential"
   handler       = "index.test"
 
