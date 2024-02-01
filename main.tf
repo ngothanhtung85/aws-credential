6ba25fa36a8ca1cc1863e31fd63dc43d52e037c0
TUNG NGO <ngothanhtung85@gmail.com>
2024-02-01 17:05:49 +0700

    update

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_lambda_function" "test_lambda" {
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  runtime = "nodejs8.10"
+  runtime = "nodejs16.x"
 
   environment {
     variables = {
